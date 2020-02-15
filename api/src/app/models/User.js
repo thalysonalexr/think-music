4187d99e105cc6b02246e065cbb60f91d68e5f75
Thalyson Rodrigues <thalysonrodrigues.dev@gmail.com>
2020-02-15 10:19:01 -0400

    user model

---
+++ api/src/app/models/User.js
@@ -0,0 +1,50 @@
+const { model, Schema } = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 255
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    maxlength: 100
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+    select: false
+  },
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user'
+  },
+  status: {
+    type: Boolean,
+    default: true
+  },
+  passwordResetToken: {
+    type: String,
+    select: false
+  },
+  passwordResetExpires: {
+    type: Date,
+    select: false
+  }
+}, { timestamps: true });
+
+UserSchema.pre('save', async function (next) {
+  const hash = await bcrypt.hash(this.password, 10);
+  this.password = hash;
+  next();
+});
+
+module.exports = model('User', UserSchema);
