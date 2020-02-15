b85e065ed5288ac7532cb0a638333d95ee7ba984
Thalyson Rodrigues <thalysonrodrigues.dev@gmail.com>
2020-02-15 10:16:49 -0400

    update index

--- api/src/index.js
+++ api/src/index.js
@@ -1,8 +1,5 @@
-const express = require('express');
-const cors = require('cors');
-const routes = require('./routes');
-
 const host = process.env.NODE_ENV || '0.0.0.0';
+const port = process.env.PORT || 3000;
 
 if ( ! process.env.NODE_ENV) {
   process.env.NODE_ENV = 'development';
@@ -11,8 +8,11 @@ if ( ! process.env.NODE_ENV) {
 require('./config');
 require('./services/mongo');
 
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
