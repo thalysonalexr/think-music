d3f3bf135958be701d767a7852d03d71ed67b73f
Thalyson Rodrigues <thalysonrodrigues.dev@gmail.com>
2020-02-15 10:17:22 -0400

    routes auth user

--- api/src/routes.js
+++ api/src/routes.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const authMiddleware = require('./app/middlewares/auth');
+const UsersController = require('./app/controllers/UsersController');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  return res.send('Hello World NodeJS!');
+  return res.status(200).json({
+    message: 'Hello! you are accessing the Think Music Project API!',
+    links: {
+      docs: 'https://github.com/thalysonalexr/think-music#README.md'
+    }
+  });
 });
+
+router.post('/auth/register', UsersController.store);
+router.post('/auth/authenticate', UsersController.auth);
+router.post('/auth/forgot_password', UsersController.forgotPassword);
+router.post('/auth/reset_password', UsersController.resetPassword);
 
 module.exports = router;
