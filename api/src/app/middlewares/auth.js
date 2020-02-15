38ed22309be88640c1c1791cd3545c066e7ecc8c
Thalyson Rodrigues <thalysonrodrigues.dev@gmail.com>
2020-02-15 10:19:24 -0400

    add middleware to intercept request not authorizate

---
+++ api/src/app/middlewares/auth.js
@@ -0,0 +1,42 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if ( ! authHeader) {
+    return res.status(401).json({
+      error: 401,
+      message: 'No token provided.'
+    });
+  }
+
+  const parts = authHeader.split(' ');
+
+  if (parts.length !== 2) {
+    return res.status(401).json({
+      error: 401,
+      message: 'Error token.'
+    });
+  }
+
+  const [ scheme, token ] = parts;
+
+  if (!/^Bearer$/i.test(scheme)) {
+    return res.status(401).json({
+      error: 401,
+      message: 'Token malformatted.'
+    });
+  }
+
+  jwt.verify(token, process.env.TM_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({
+        error: 401,
+        message: 'Token invalid.'
+      });
+    }
+
+    req.userId = decoded.id;
+    return next();
+  });
+};
