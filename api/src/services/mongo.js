72c3cf0de27144e64f665aef9a65401421f06e86
Thalyson Rodrigues <thalysonrodrigues.dev@gmail.com>
2020-02-15 10:17:44 -0400

    add config

--- api/src/services/mongo.js
+++ api/src/services/mongo.js
@@ -3,4 +3,5 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.TM_MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useFindAndModify: false
 }, () => console.log('Connected to MongoDB'));
