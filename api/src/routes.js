e9eb2410925c8d4af17c50a675d536fffaddf58a
Thalyson Rodrigues <thalysonrodrigues.dev@gmail.com>
2020-02-27 21:25:03 -0400

    add routes users and admin; refatoring import; add middlewares

--- api/src/routes.js
+++ api/src/routes.js
@@ -1,8 +1,10 @@
-import express from 'express';
+import { Router } from 'express';
 import authMiddleware from './app/middlewares/auth';
+import authorizationMiddleware from './app/middlewares/authorization';
+import unprocessableMiddleware from './app/middlewares/unprocessable';
 import UsersController from './app/controllers/UsersController';
 
-const router = express.Router();
+const router = Router();
 
 router.get('/', (req, res) => {
   return res.status(200).json({
@@ -18,7 +20,42 @@ router.post('/auth/authenticate', UsersController.auth);
 router.post('/auth/forgot_password', UsersController.forgotPassword);
 router.post('/auth/reset_password', UsersController.resetPassword);
 
-router.get('/users', authMiddleware, UsersController.index);
-router.get('/users/:id', authMiddleware, UsersController.show);
+router.get('/users',
+  authMiddleware,
+  unprocessableMiddleware,
+  UsersController.index
+);
+
+router.get('/users/:id',
+  authMiddleware,
+  unprocessableMiddleware,
+  UsersController.show
+);
+
+router.put('/users/:id',
+  authMiddleware,
+  unprocessableMiddleware,
+  UsersController.update
+);
+
+router.delete('/users/:id',
+  authMiddleware,
+  unprocessableMiddleware,
+  UsersController.destroy
+);
+
+router.post('/users/admin',
+  authMiddleware,
+  unprocessableMiddleware,
+  authorizationMiddleware,
+  UsersController.store
+);
+
+router.post('/users/:id/enable',
+  authMiddleware,
+  unprocessableMiddleware,
+  authorizationMiddleware,
+  UsersController.disableUser
+);
 
 export default router;
