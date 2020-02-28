d872c8559321ac34c808d52fbcd95705b4a09d73
Thalyson Rodrigues <thalysonrodrigues.dev@gmail.com>
2020-02-27 21:26:05 -0400

    add middleware to check user is active

---
+++ api/src/app/middlewares/unprocessable.js
@@ -0,0 +1,15 @@
+import User from '../models/User';
+
+const unprocessableMiddleware = async (req, res, next) => {
+  const user = await User.findById(req.userId);
+
+  if (user && user.status)
+    return next();
+
+  return res.status(422).json({
+    error: 422,
+    message: 'This user are inactive or latest removed.'
+  });
+}
+
+export default unprocessableMiddleware;
