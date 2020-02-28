db861e83eaae5464babe345445af73d16493d71d
Thalyson Rodrigues <thalysonrodrigues.dev@gmail.com>
2020-02-27 21:25:45 -0400

    add middleware to authorizate admin

---
+++ api/src/app/middlewares/authorization.js
@@ -0,0 +1,20 @@
+import User from '../models/User';
+
+const authorizationMiddleware = async (req, res, next) => {
+  const user = await User.findOne({
+    _id: req.userId,
+    role: 'admin'
+  });
+
+  if (user && user.role === 'admin') {
+    req.role = 'admin';
+    return next();
+  }
+
+  return res.status(403).json({
+    error: 403,
+    message: 'Forbidden the access to resource.'
+  });
+}
+
+export default authorizationMiddleware;
