d922779d3fe501e005497635a8c2b302c5bc3b58
Thalyson Rodrigues <thalysonrodrigues.dev@gmail.com>
2020-03-12 21:26:37 -0400

    config associate users and revoked tokens

--- api/src/services/database/index.js
+++ api/src/services/database/index.js
@@ -2,9 +2,14 @@ import Sequelize from 'sequelize';
 import config from '../../config/database';
 
 import User from '../../app/models/User';
+import RevokedTokens from '../../app/models/RevokedTokens';
 
 const connection = new Sequelize(config[process.env.NODE_ENV]);
 
 User.init(connection);
+RevokedTokens.init(connection);
+
+User.associate(connection.models);
+RevokedTokens.associate(connection.models);
 
 export default connection;
