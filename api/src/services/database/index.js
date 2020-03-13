615c83b95c5c3114f02a8210f54edcb98fc5229d
Thalyson Rodrigues <thalysonrodrigues.dev@gmail.com>
2020-03-12 20:14:04 -0400

    move database and migrations

--- api/src/services/database/index.js
+++ api/src/services/database/index.js
@@ -1,6 +1,10 @@
 import Sequelize from 'sequelize';
-import config from '../config/database';
+import config from '../../config/database';
+
+import User from '../../app/models/User';
 
 const connection = new Sequelize(config[process.env.NODE_ENV]);
+
+User.init(connection);
 
 export default connection;
