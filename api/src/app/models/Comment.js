8bd334544f9908e996dc02366d8facccf766e35d
Thalyson Rodrigues <thalysonrodrigues.dev@gmail.com>
2020-03-12 22:49:09 -0400

    add model to comment

---
+++ api/src/app/models/Comment.js
@@ -0,0 +1,16 @@
+import { DataTypes, Model } from 'sequelize';
+
+class Comment extends Model {
+  static init(sequelize) {
+    super.init({
+      comment: DataTypes.STRING(280),
+    }, { sequelize });
+  }
+
+  static associate(models) {
+    this.belongsTo(models.Interpretation, { foreignKey: 'interpretation_id', as: 'comments_interpretation' });
+    this.belongsTo(models.User, { foreignKey: 'user_id', as: 'comments_user' });
+  }
+}
+
+export default Comment;
