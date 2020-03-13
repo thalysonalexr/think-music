9139e95ae9fa9558a6029d7c68d124290da28d62
Thalyson Rodrigues <thalysonrodrigues.dev@gmail.com>
2020-03-12 22:21:36 -0400

    add categories model

---
+++ api/src/app/models/Categories.js
@@ -0,0 +1,16 @@
+import { DataTypes, Model } from 'sequelize';
+
+class Categories extends Model {
+  static init(sequelize) {
+    super.init({
+      title: DataTypes.STRING(45),
+      description: DataTypes.STRING(255),
+    }, { sequelize });
+  }
+
+  static associate(models) {
+    this.hasMany(models.Musics, { foreignKey: 'category_id', as: 'musics_category' });
+  }
+}
+
+export default Categories;
--- api/src/services/database/index.js
+++ api/src/services/database/index.js
@@ -3,13 +3,16 @@ import config from '../../config/database';
 
 import User from '../../app/models/User';
 import RevokedTokens from '../../app/models/RevokedTokens';
+import Categories from '../../app/models/Categories';
 
 const connection = new Sequelize(config[process.env.NODE_ENV]);
 
 User.init(connection);
 RevokedTokens.init(connection);
+Categories.init(connection);
 
 User.associate(connection.models);
 RevokedTokens.associate(connection.models);
+Categories.associate(connection.models);
 
 export default connection;
