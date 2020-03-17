5b117e254a83e1e8aa5f7bf47b3c844b22a3b4d6
Thalyson Rodrigues <thalysonrodrigues.dev@gmail.com>
2020-03-17 18:36:16 -0400

    refactoring

--- api/src/app/models/Like.js
+++ api/src/app/models/Like.js
@@ -1,8 +1,17 @@
 import { DataTypes, Model } from 'sequelize';
+import { paginate } from 'sequelize-paginate';
 
 class Like extends Model {
   static init(sequelize) {
     super.init({
+      interpretation_id: {
+        type: DataTypes.UUID,
+        primaryKey: true
+      },
+      user_id: {
+        type: DataTypes.UUID,
+        primaryKey: true
+      },
       like: DataTypes.BOOLEAN,
       dislike: DataTypes.BOOLEAN,
     }, {
@@ -12,9 +21,11 @@ class Like extends Model {
   }
 
   static associate(models) {
-    this.belongsTo(models.User, { foreignKey: 'user_id', as: 'likes_user' });
-    this.belongsTo(models.Interpretation, { foreignKey: 'interpretation_id', as: 'likes_interpretation' });
+    this.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
+    this.belongsTo(models.Interpretation, { foreignKey: 'interpretation_id', as: 'interpretation' });
   }
 }
+
+paginate(Like);
 
 export default Like;
