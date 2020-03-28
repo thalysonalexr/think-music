a88efe2ac339897e1ecab4e27679456f528fae35
Thalyson Rodrigues <thalysonrodrigues.dev@gmail.com>
2020-03-28 04:28:06 -0400

    separate server on app

---
+++ api/src/app.js
@@ -0,0 +1,26 @@
+if (!process.env.NODE_ENV) {
+  process.env.NODE_ENV = 'development';
+}
+
+import './config';
+import './services/mongo';
+import './services/database';
+
+import express from 'express';
+import cors from 'cors';
+import expressIp from 'express-ip';
+import expressUserAgent from 'express-useragent';
+import Middlewares from './middlewares';
+import routes from './routes';
+
+const app = express();
+
+app.use(cors());
+app.use(expressIp().getIpInfoMiddleware);
+app.use(expressUserAgent.express());
+app.use(Middlewares.accesslog);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use('/v1', routes);
+
+export default app;
--- api/src/server.js
+++ api/src/server.js
@@ -1,30 +1,5 @@
-if ( ! process.env.NODE_ENV) {
-  process.env.NODE_ENV = 'development';
-}
-
-import './config';
-import './services/mongo';
-import './services/database';
-
 const port = process.env.PORT;
 const host = process.env.BASE_URL;
-
-import express from 'express';
-import cors from 'cors';
-import expressIp from 'express-ip';
-import expressUserAgent from 'express-useragent';
-import Middlewares from './middlewares';
-import routes from './routes';
-
-const app = express();
-
-app.use(cors());
-app.use(expressIp().getIpInfoMiddleware);
-app.use(expressUserAgent.express());
-app.use(Middlewares.accesslog);
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use('/v1', routes);
 
 app.listen(port, host, () => {
   console.log(`[${process.env.NODE_ENV}] Running on http://${host}:${port}`);
